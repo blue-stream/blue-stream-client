3580c8a931603dc32ea369424962e9f231cda6c4
Ron Borysovski <roni537@gmail.com>
2018-05-21 13:03:49 +0300

    style(imports): Remove unused import

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { VideosComponent } from './videos/videos.component';
 import { VideoSectionListComponent } from './video-section-list/video-section-list.component';
 
 const routes: Routes = [
