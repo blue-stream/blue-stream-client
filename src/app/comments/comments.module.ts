fc95c7a2f46e981422975a822b91010a3c5397b4
AlmogVC <almogvc@gmail.com>
2018-05-21 13:11:40 +0300

    chore(Comment): Add comment action buttons

--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -4,13 +4,19 @@ import { CommentsComponent } from './comments.component';
 import { CommentComponent } from './comment-list/comment/comment.component';
 import { SharedModule } from '../shared/shared.module';
 import { CommentListComponent } from './comment-list/comment-list.component';
+import { CommentActionButtonsComponent } from './comment-list/comment/comment-action-buttons/comment-action-buttons.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [CommentsComponent, CommentComponent, CommentListComponent],
+  declarations: [
+    CommentsComponent,
+    CommentComponent,
+    CommentListComponent,
+    CommentActionButtonsComponent
+  ],
   exports: [CommentsComponent]
 })
 export class CommentsModule { }
