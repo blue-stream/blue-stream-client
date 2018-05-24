f9ebd43db1e51174994fb8b78672bb43777e8d30
Ron Borysovski <roni537@gmail.com>
2018-05-24 09:50:43 +0300

    chore(avatar): Create generic avatar component

---
+++ src/app/shared/avatar/avatar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+@Component({
+  selector: 'bs-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss']
+})
+export class AvatarComponent implements OnInit {
+
+  @Input() source: string;
+  @Input() size: 'x-small' | 'small' | 'medium' | 'large' = 'medium';
+  @Input() alt?: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
