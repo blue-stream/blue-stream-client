5f15bab9ce95e5979ee67da764c41ee7083bce5c
Ron Borysovski <roni537@gmail.com>
2018-05-27 17:51:22 +0300

    chore: Add action functionalities to video actions

--- src/app/shared/video-player/video-actions/video-actions.component.ts
+++ src/app/shared/video-player/video-actions/video-actions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-
+import { MatSliderChange } from '@angular/material/slider';
+import * as moment from 'moment';
+import * as format from 'format-duration';
 @Component({
   selector: 'bs-video-actions',
   templateUrl: './video-actions.component.html',
@@ -10,18 +12,46 @@ export class VideoActionsComponent implements OnInit {
   @Input() volume: number;
   @Input() currentTime;
   @Input() duration;
+  @Input() fullscreen: boolean;
+  @Input() playing: boolean;
   @Output() volumeChange: EventEmitter<number>;
   @Output() toggleVideo: EventEmitter<void>;
+  @Output() toggleFullscreen: EventEmitter<void>;
 
   constructor() {
     this.volumeChange = new EventEmitter<number>();
     this.toggleVideo = new EventEmitter<void>();
+    this.toggleFullscreen = new EventEmitter<void>();
   }
 
   ngOnInit() {
     console.log('volume', this.volume);
     console.log('currentTime', this.currentTime);
     console.log('duration', this.duration);
+  }
+
+  mute() {
+    if (this.volume !== 0) {
+      this.volumeChange.next(0);
+    } else {
+      this.volumeChange.next(1);
+    }
+  }
+
+  toggleVideoClick() {
+    this.toggleVideo.next();
+  }
+
+  toggleFullscreenClick() {
+    this.toggleFullscreen.next();
+  }
+
+  volumeSliderChange(event: MatSliderChange) {
+    this.volumeChange.next(event.value);
+  }
+
+  secondsToTime(seconds) {
+    return format(seconds * 1000);
   }
 
 }
--- src/app/shared/video-player/video-player.component.ts
+++ src/app/shared/video-player/video-player.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import fscreen from 'fscreen';
 
 @Component({
   selector: 'bs-video-player',
@@ -8,24 +9,42 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  @Input() source = 'assets/video2.mp4';
+  @Input() source = 'assets/video.mp4';
   @ViewChild('video') video: ElementRef;
+  fullscreen: boolean = false;
+  playing: boolean = true;
+  volume: number;
   progress = 0;
 
-  constructor(private sanitizer: DomSanitizer) {
-  }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.volume = this.video.nativeElement.volume;
+  }
+
+  toggleFullscreen() {
+    if (!fscreen.fullscreenElement) {
+      fscreen.requestFullscreen(this.video.nativeElement.parentElement);
+      this.fullscreen = true;
+    } else {
+      fscreen.exitFullscreen();
+      this.fullscreen = false;
+    }
   }
 
   toggleVideo() {
     const video = this.video.nativeElement;
-    console.log(video.currentTime);
     if (video.paused) {
       video.play();
+      this.playing = true;
     } else {
       video.pause();
+      this.playing = false;
     }
+  }
+
+  volumeChange(volume: number) {
+    this.video.nativeElement.volume = volume;
   }
 
   updateProgress() {
@@ -34,7 +53,6 @@ export class VideoPlayerComponent implements OnInit {
   }
 
   progressChanged(progress: number) {
-    console.log(progress);
     const video = this.video.nativeElement;
     this.progress = progress;
     video.currentTime = video.duration * progress / 100;
