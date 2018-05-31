985376812415dd5edd05b0a918f37dbd297003eb
Ron Borysovski <roni537@gmail.com>
2018-05-31 15:10:59 +0300

    fix(video-player): Video seeking jump by 1% instead of 1 second

--- src/app/shared/video-player/video-player.component.ts
+++ src/app/shared/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import fscreen from 'fscreen';
 import { Video } from '../models/video.model';
@@ -6,6 +6,7 @@ import { BehaviorSubject } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+
 @Component({
   selector: 'bs-video-player',
   templateUrl: './video-player.component.html',
@@ -15,19 +16,24 @@ export class VideoPlayerComponent implements OnInit {
 
   @Input() video: Video;
   @ViewChild('videoPlayer') videoPlayer: ElementRef;
+  @Output() toggleWideScreen: EventEmitter<boolean>;
+  private playPromise: Promise<void>;
+  private pausePromise: Promise<void>;
   mouseMove: BehaviorSubject<void>;
+  previousVideoState: boolean | null = null;
   hideActions: boolean = false;
   fullscreen: boolean = false;
   volume: number;
   progress = 0;
 
   constructor(private sanitizer: DomSanitizer) {
+    this.toggleWideScreen = new EventEmitter<boolean>();
     this.mouseMove = new BehaviorSubject<void>(null);
     this.mouseMove
       .map(() => {
         this.hideActions = false;
       })
-      .debounceTime(3000)
+      .debounceTime(300000)
       .subscribe(() => {
         this.hideActions = true;
       });
@@ -50,10 +56,22 @@ export class VideoPlayerComponent implements OnInit {
   toggleVideo() {
     const video = this.videoPlayer.nativeElement;
     if (video.paused) {
-      video.play();
+      this.playVideo();
     } else {
-      video.pause();
+      this.pauseVideo();
     }
+  }
+
+  pauseVideo() {
+    Promise.all([this.playPromise, this.pausePromise]).then(() => {
+      this.pausePromise = this.videoPlayer.nativeElement.pause();
+    });
+  }
+
+  playVideo() {
+    Promise.all([this.playPromise, this.pausePromise]).then(() => {
+      this.playPromise = this.videoPlayer.nativeElement.play();
+    });
   }
 
   volumeChange(volume: number) {
@@ -63,6 +81,21 @@ export class VideoPlayerComponent implements OnInit {
   updateProgress() {
     const { currentTime, duration } = this.videoPlayer.nativeElement;
     this.progress = Math.floor((100 / duration) * currentTime);
+  }
+
+  onProgressChangeStart() {
+    const video = this.videoPlayer.nativeElement as HTMLVideoElement;
+    if (this.previousVideoState === null) {
+      this.previousVideoState = !video.paused;
+    }
+    this.pauseVideo();
+  }
+
+  onProgressChangeEnd() {
+    if (this.previousVideoState !== null) {
+      this.previousVideoState ? this.playVideo() : this.pauseVideo();
+      this.previousVideoState = null;
+    }
   }
 
   progressChanged(progress: number) {
--- src/app/shared/video-player/video-progress/video-progress.component.ts
+++ src/app/shared/video-player/video-progress/video-progress.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { MatSliderChange } from '@angular/material/slider';
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/throttleTime';
 
 @Component({
   selector: 'bs-video-progress',
@@ -9,18 +11,34 @@ import { MatSliderChange } from '@angular/material/slider';
 export class VideoProgressComponent implements OnInit {
 
   @Input() value = 0;
+  @Input() duration;
   @ViewChild('videoProgress') videoProgress: ElementRef;
+  @Output() progressChangeStart: EventEmitter<void>;
   @Output() progressChanged: EventEmitter<number>;
+  @Output() progressChangeEnd: EventEmitter<void>;
+  progressSliderChange: Subject<MatSliderChange>;
 
   constructor() {
     this.progressChanged = new EventEmitter<number>();
+    this.progressChangeStart = new EventEmitter<void>();
+    this.progressChangeEnd = new EventEmitter<void>();
+    this.progressSliderChange = new Subject<MatSliderChange>();
+    this.progressSliderChange
+      .map((event: MatSliderChange) => {
+        return event.value;
+      })
+      .subscribe((progress: number) => {
+        this.progressChangeStart.next();
+        if (Math.abs(progress - this.value) >= 1) {
+          this.progressChanged.next(progress);
+        }
+      });
+  }
+
+  onProgressChange() {
+    this.progressChangeEnd.next();
   }
 
   ngOnInit() {
   }
-
-  changeTime(event: MatSliderChange) {
-    this.progressChanged.next(event.value);
-  }
-
 }
