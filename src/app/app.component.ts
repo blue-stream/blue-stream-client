d27b44aa61d54703d80fd0329bb29dc0aa90af55
AlmogVC <almogvc@gmail.com>
2018-06-07 11:15:13 +0300

    fix(SideNav): Change mode by page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouteSpecificConfig, SideNavMode, routeConfig } from './route-specific.config';
 
 @Component({
   selector: 'bs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
+  router: Router;
+  isSideNavOpen: boolean = true;
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private _router: Router) {
+    this.router = _router;
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -19,5 +25,25 @@ export class AppComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  getSideNavMode(): SideNavMode {
+    let url: string = '';
+    const endIndex = this.router.url.slice(1, this.router.url.length).indexOf('/');
+
+    if (endIndex) {
+      url = this.router.url.slice(0, endIndex + 1);
+    } else {
+      url = this.router.url.slice(0, this.router.url.length);
+    }
+
+    if (routeConfig[url].sideNavMode === 'side') {
+      this.isSideNavOpen = !this.mobileQuery.matches;
+      return this.mobileQuery.matches ? 'over' : 'side';
+    } else {
+      this.isSideNavOpen = routeConfig[url].isNavDefaultlyOpen;
+    }
+
+    return routeConfig[url].sideNavMode;
   }
 }
---
+++ src/app/route-specific.config.ts
@@ -0,0 +1,21 @@
+export type SideNavMode = 'over' | 'push' | 'side';
+
+interface RouteConfig {
+    sideNavMode: SideNavMode;
+    isNavDefaultlyOpen: boolean;
+}
+
+export interface RouteSpecificConfig {
+    [route: string]: RouteConfig;
+}
+
+export const routeConfig: RouteSpecificConfig = {
+    '/watch': {
+        sideNavMode: 'over',
+        isNavDefaultlyOpen: false
+    },
+    '': {
+        sideNavMode: 'side',
+        isNavDefaultlyOpen: true
+    }
+}
