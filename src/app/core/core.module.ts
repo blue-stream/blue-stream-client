6cedc3f3cff3a9ca2f5dabb1069ac0ef3026bfbd
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-01 13:54:30 +0200

    build: Remove unnecessary module import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
 import { VideoService } from './services/video.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule,
   ],
   declarations: [],
   providers: [
