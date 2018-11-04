29bb0643c438251d9de488484f54a567e39ec65e
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-04 16:27:57 +0200

    refactor: Add reactive form to upload form

--- src/app/upload/video-upload-form/video-upload-form.component.ts
+++ src/app/upload/video-upload-form/video-upload-form.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material';
 import { FileUpload } from '../file-upload';
+import { Video } from 'src/app/shared/models/video.model';
+
 
 @Component({
   selector: 'bs-video-upload-form',
@@ -10,10 +15,56 @@ export class VideoUploadFormComponent implements OnInit {
 
   @Input() isPublishReady: boolean;
   @Input('file') file: FileUpload;
+  @Output() videoSubmitted: EventEmitter<Video> = new EventEmitter();
 
-  constructor() { }
+  uploadForm: FormGroup;
+  separatorKeysCodes = [ENTER, COMMA];
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  createForm() {
+    this.uploadForm = this.fb.group({
+      title: this.fb.control(this.file.file.name),
+      description: this.fb.control(''),
+      tags: this.fb.array([]),
+    });
+  }
+
+  removeTag(index: number): void {
+    const tags = this.uploadForm.get('tags') as FormArray;
+
+    if (index >= 0) {
+      tags.removeAt(index);
+    }
+  }
+
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+        const tags = this.uploadForm.get('tags') as FormArray;
+        tags.push(this.fb.control(value.trim()));
+    }
+
+    if (input) {
+        input.value = '';
+    }
+}
+
+  onSubmit(event: Event) {
+    this.videoSubmitted.emit(this.uploadForm.value);
+  }
 
   ngOnInit() {
+    this.uploadForm = new FormGroup({
+      title: new FormControl(this.file.file.name),
+      description: new FormControl(''),
+      tags: new FormControl(''),
+    });
+
+    this.createForm();
   }
 
 }
