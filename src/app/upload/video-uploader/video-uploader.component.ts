c9148191922b1cb9262d65a853a19f4e377948f8
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-07 10:00:50 +0200

    refactor: Remove publish from video uploader component

--- src/app/upload/video-uploader/video-uploader.component.ts
+++ src/app/upload/video-uploader/video-uploader.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { FileUpload } from '../file-upload';
 import { FileUploaderService } from '../file-uploader.service';
 import { VideoService } from '../../core/services/video.service';
 import { Video } from '../../shared/models/video.model';
 import { VideoUpload } from '../video-upload.interface';
 import { forkJoin, of, interval } from 'rxjs';
 import { MatSnackBar } from '@angular/material';
-import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -20,25 +18,10 @@ export class VideoUploaderComponent implements OnInit {
 
   constructor(
     private fileUploaderService: FileUploaderService,
-    private videoService: VideoService,
-    public snackBar: MatSnackBar,
-    private translateService: TranslateService) { }
+    private videoService: VideoService) { }
 
   ngOnInit() {
     this.videoUploadQueue = this.fileUploaderService.getQueue();
-  }
-
-  publishVideo(video: Video) {
-    this.videoService.update(video).subscribe(updatedVideo => {
-      this.translateService.get([
-        'UPLOADER.VIDEO_UPLOADER.PUBLISH_SUCCESS',
-        'UPLOADER.VIDEO_UPLOADER.PUBLISH_SUCCESS_APPROVAL']).subscribe(translations => {
-          this.snackBar.open(
-            translations['UPLOADER.VIDEO_UPLOADER.PUBLISH_SUCCESS'],
-            translations['UPLOADER.VIDEO_UPLOADER.PUBLISH_SUCCESS_APPROVAL'],
-            { duration: 2000 });
-        });
-    });
   }
 
   filesSelected(files: FileList) {
