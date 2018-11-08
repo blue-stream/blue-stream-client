b6c8d94002af8b2178d0906a6cfc12f37d8a2876
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-08 11:26:37 +0200

    Refactor: Change video cancellation implementation location

--- src/app/upload/upload-progress-bar/upload-progress-bar.component.ts
+++ src/app/upload/upload-progress-bar/upload-progress-bar.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FileUpload } from '../file-upload';
 import { FileUploadStatus } from '../file-upload-status.enum';
-import { FileUploaderService } from '../file-uploader.service';
-import { MatDialog } from '@angular/material/dialog';
-import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
-import { ConfirmDialogData } from '../../shared/confirm-dialog/confirm-dialog-data';
 
 @Component({
   selector: 'bs-upload-progress-bar',
@@ -16,27 +12,5 @@ export class UploadProgressBarComponent {
   @Input('file') file: FileUpload;
   fileUploadStatus = FileUploadStatus;
 
-  constructor(
-    private fileUploaderService: FileUploaderService,
-    private dialog: MatDialog
-  ) { }
-
-  cancelUpload() {
-    const confirmDialog = this.dialog.open(ConfirmDialogComponent, {
-      width: '350px',
-      data: {
-        title: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.TITLE',
-        text: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.TEXT',
-        cancelButtonText: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.CANCEL_BUTTON',
-        confirmButtonText: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.CONFIRM_BUTTON'
-      } as ConfirmDialogData
-    });
-
-    confirmDialog.afterClosed().subscribe((confirm: boolean) => {
-      if (confirm) {
-        this.fileUploaderService.cancel(this.file);
-      }
-    });
-
-  }
+  constructor() { }
 }
--- src/app/upload/video-upload-progress/video-upload-progress.component.ts
+++ src/app/upload/video-upload-progress/video-upload-progress.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { FileUpload } from '../file-upload';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FileUploadStatus } from '../file-upload-status.enum';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogData } from '../../shared/confirm-dialog/confirm-dialog-data';
+import { FileUploaderService } from '../file-uploader.service';
 
 @Component({
   selector: 'bs-video-upload-progress',
@@ -17,11 +21,33 @@ export class VideoUploadProgressComponent implements OnInit {
 
   @ViewChild('videoPlayer') videoPlayer: ElementRef;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer,
+    private fileUploaderService: FileUploaderService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.videoPlayer.nativeElement.muted = 'muted';
     this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(this.uploadFile.file));
+  }
+
+  cancelUpload() {
+    const confirmDialog = this.dialog.open(ConfirmDialogComponent, {
+      width: '350px',
+      data: {
+        title: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.TITLE',
+        text: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.TEXT',
+        cancelButtonText: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.CANCEL_BUTTON',
+        confirmButtonText: 'UPLOADER.UPLOAD_PROGRESS_BAR.CANCEL_UPLOAD_DIALOG.CONFIRM_BUTTON'
+      } as ConfirmDialogData
+    });
+
+    confirmDialog.afterClosed().subscribe((confirm: boolean) => {
+      if (confirm) {
+        this.fileUploaderService.cancel(this.uploadFile);
+      }
+    });
+
   }
 
 }
