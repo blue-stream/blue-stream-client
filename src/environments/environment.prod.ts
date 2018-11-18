7b89fa8cc7c16b6ad99cecc46a668e1fd33c0c12
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-18 17:10:23 +0200

    build: Add comment micro serice url to env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   videoServiceUrl: 'http://localhost:5001/',
   uploadServiceUrl: 'http://localhost:5002/',
+  commentServiceUrl: 'http://localhost:5003/',
   titleMinLength: 3,
   titleMaxLength: 256,
   descriptionMaxLength: 5000,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   videoServiceUrl: 'http://localhost:5001/',
   uploadServiceUrl: 'http://localhost:5002/',
+  commentServiceUrl: 'http://localhost:5003/',
   titleMinLength: 3,
   titleMaxLength: 256,
   descriptionMaxLength: 5000,
