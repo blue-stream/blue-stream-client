f5477928f413557dacd0d8fedac792888277d235
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-19 16:27:23 +0200

    refactor: Add comment form

--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -7,18 +7,22 @@ import { CommentListComponent } from './comment-list/comment-list.component';
 import { CommentActionButtonsComponent } from './comment-list/comment/comment-action-buttons/comment-action-buttons.component';
 import { CommentsHeaderComponent } from './comments-header/comments-header.component';
 import { CommentService } from './comment.service';
+import { CommentFormComponent } from './comment-form/comment-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     CommentsComponent,
     CommentComponent,
     CommentListComponent,
     CommentActionButtonsComponent,
-    CommentsHeaderComponent
+    CommentsHeaderComponent,
+    CommentFormComponent
   ],
   exports: [CommentsComponent],
   providers: [CommentService]
