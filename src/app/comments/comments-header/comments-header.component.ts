89c1bba3b4505c581b0c06e76333718749ae312c
SirSaar <saar643@gmail.com>
2018-12-19 12:55:26 +0200

    feat: add input for dynamic videoId

--- src/app/comments/comments-header/comments-header.component.ts
+++ src/app/comments/comments-header/comments-header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CommentsHeaderComponent implements OnInit {
   @Input() commentsAmount: number;
+  @Input() resource: string;
   constructor() { }
 
   ngOnInit() {
