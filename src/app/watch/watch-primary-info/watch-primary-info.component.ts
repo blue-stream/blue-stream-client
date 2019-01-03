6a5af92da8f6f5e8c134b27eddb334493887338f
SirSaar <saar643@gmail.com>
2019-01-03 17:30:51 +0200

    refactor: move edit actions to primary-info component

--- src/app/watch/watch-primary-info/watch-primary-info.component.ts
+++ src/app/watch/watch-primary-info/watch-primary-info.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Video } from '../../shared/models/video.model';
 import { ReactionService } from 'src/app/core/services/reaction.service';
 import { Reaction, ResourceType, ReactionType } from 'src/app/shared/models/reaction.model';
+import { Router } from '@angular/router';
+import { VideoService } from '../../core/services/video.service';
+import { TranslateService } from '@ngx-translate/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'bs-watch-primary-info',
@@ -14,8 +18,39 @@ export class WatchPrimaryInfoComponent implements OnInit {
   currentReactionType: ReactionType;
   resourceType: ResourceType = ResourceType.Video;
 
-  constructor(private reactionService: ReactionService) { }
+  constructor(
+    private reactionService: ReactionService,
+    private videoService: VideoService,
+    private translateService: TranslateService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onDeleteVideo() {
+    this.translateService.get('WATCH.WATCH_PRIMARY_INFO.DELETE.CONFIRM_MESSAGE')
+    .subscribe(translation => {
+        if (confirm(translation)) {
+            return this.deleteVideo();
+        } else {
+            return false;
+        }
+    });
+  }
+
+  deleteVideo() {
+    this.videoService.delete(this.video.id).subscribe(video => {
+      this.translateService.get([
+        'WATCH.WATCH_PRIMARY_INFO.DELETE.DELETE_SUCCESS',
+        'WATCH.WATCH_PRIMARY_INFO.DELETE.DELETE_SUCCESS_APPROVAL']).subscribe(translations => {
+          this.snackBar.open(
+            translations['WATCH.WATCH_PRIMARY_INFO.DELETE.DELETE_SUCCESS'],
+            translations['WATCH.WATCH_PRIMARY_INFO.DELETE.DELETE_SUCCESS_APPROVAL'],
+            { duration: 2000 });
+          this.router.navigate(['/']);
+        });
+    });
   }
 }
