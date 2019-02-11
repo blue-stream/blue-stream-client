9f616110f954061b6385e3af79112a415eb7609f
Ron Borysovski <roni537@gmail.com>
2019-02-11 10:23:44 +0200

    style: Add return types

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from 'src/environments/environment';
+import { User } from './models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,17 @@ export class UserService {
   constructor(private cookieService: CookieService) {
   }
 
-  public get isAuthenticated() {
+  public get isAuthenticated(): boolean {
     return this.cookieService.check(environment.authenticationToken);
   }
 
-  public get currentUser() {
+  public get currentUser(): User {
     const token = this.cookieService.get(environment.authenticationToken);
     if (token) {
       const tokenData = token.split('.')[1];
       const decodedData = window.atob(tokenData);
 
-      return JSON.parse(decodedData);
+      return JSON.parse(decodedData) as User;
     }
 
     return null;
