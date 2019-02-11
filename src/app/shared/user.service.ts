62819bc9ee90ac92e3dda66907fde626260aa011
Ron Borysovski <roni537@gmail.com>
2019-02-11 10:16:56 +0200

    refactor: Add `currentUser` and `isAuthenticated` getters

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() { }
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  public get isAuthenticated() {
+    return this.cookieService.check(environment.authenticationToken);
+  }
+
+  public get currentUser() {
+    const token = this.cookieService.get(environment.authenticationToken);
+    if (token) {
+      const tokenData = token.split('.')[1];
+      const decodedData = window.atob(tokenData);
+
+      return JSON.parse(decodedData);
+    }
+
+    return null;
+  }
 
   getUser() {
     return 'user@domain';
