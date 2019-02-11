c5dd5f90d5efd44a923c9493f8054583385fb0ac
Ron Borysovski <roni537@gmail.com>
2019-02-11 10:15:49 +0200

    refactor: Get token from cookie instead of mock token

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,14 +1,19 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    constructor(private cookieService: CookieService) { }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders: {
                 'Authorization':
-                    `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXJAZG9tYWluIn0.-qD7CWPcYTPAeU2bgQwQXW9QThkMoQc2e9HZAV0pHek`,
+                    `Bearer ${this.cookieService.get(environment.authenticationToken)}`,
             },
         });
 
