2d6c412210c8a969c9ed75a2be084373f0c69914
Ron Borysovski <roni537@gmail.com>
2019-03-12 16:12:54 +0200

    refactor: conditionally add image

--- src/app/shared/no-videos-found/no-videos-found.component.ts
+++ src/app/shared/no-videos-found/no-videos-found.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NoVideosFoundComponent {
 
+  @Input() showImage: boolean = false;
   @Input() message: string;
 
   constructor() { }
