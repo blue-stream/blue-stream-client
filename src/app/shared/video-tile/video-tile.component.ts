0742cef3b5aa414812d9500a956e5e991f1604a5
Almog Vagman Ciprut <almogvc@gmail.com>
2019-05-26 16:55:29 +0300

    refactor: handle history video

--- src/app/shared/video-tile/video-tile.component.ts
+++ src/app/shared/video-tile/video-tile.component.ts
@@ -13,24 +13,38 @@ export class VideoTileComponent implements OnInit {
   @Input() horizontal = false;
   @Input() description = false;
   @Input() video: Video;
+  @Input() isHistoryVideo: boolean = false;
+
   image: string;
   videoStatus = VideoStatus;
   channelName: string;
   videoPublishTime: string;
+  lastWatchTime: string;
+  timesWatched: number;
 
   constructor(private translateService: TranslateService) { }
 
   ngOnInit() {
+    if (this.isHistoryVideo && !this.video.id) {
+      this.video.title = '[Video Removed]';
+    }
+
     this.setImage(this.video.thumbnailPath);
 
     if (this.video && this.video.channel && (this.video.channel as Channel).name) {
       this.channelName = (this.video.channel as Channel).name;
     }
-    this.videoPublishTime = moment(this.video.publishDate).locale(this.translateService.currentLang).fromNow();
+
+    if (this.isHistoryVideo) {
+      this.lastWatchTime = moment(this.video.lastViewDate).locale(this.translateService.currentLang).fromNow();
+      this.timesWatched = this.video.userWatchCount;
+    } else {
+      this.videoPublishTime = moment(this.video.publishDate).locale(this.translateService.currentLang).fromNow();
+    }
   }
 
   mouseEnter() {
-    this.image = this.video.previewPath;
+    this.setImage(this.video.previewPath);
   }
 
   mouseLeave() {
@@ -38,11 +52,12 @@ export class VideoTileComponent implements OnInit {
   }
 
   setImage(source: string) {
-    if (this.video.status !== VideoStatus.READY || source.endsWith('undefined')) {
+    if (!this.video.id) {
+      this.image = '\\assets\\images\\no_thumbnail.jpg';
+    } else if (this.video.status !== VideoStatus.READY || source.endsWith('undefined')) {
       this.image = null;
     } else {
       this.image = source;
     }
   }
-
 }
