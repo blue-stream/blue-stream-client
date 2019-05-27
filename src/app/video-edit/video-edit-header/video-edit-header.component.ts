f42715ef97ca76fb8a589bab2d1a478b5411dc0f
Almog Vagman Ciprut <almogvc@gmail.com>
2019-05-27 14:51:34 +0300

    fix: display file reupload button only for owners and admins

--- src/app/video-edit/video-edit-header/video-edit-header.component.ts
+++ src/app/video-edit/video-edit-header/video-edit-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Video } from '../../shared/models/video.model';
+import { ChannelPermissionsService } from 'src/app/channels/channel-permissions.service';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'bs-video-edit-header',
@@ -11,9 +13,22 @@ export class VideoEditHeaderComponent implements OnInit {
   @Input() video: Video;
   @Input() isEdit: boolean = false;
 
-  constructor() { }
+  hasReUploadPermissions: boolean = false;
+
+  constructor(private channelPermissionsService: ChannelPermissionsService, private userService: UserService) { }
 
   ngOnInit() {
-  }
+    let channelId;
 
+    if (typeof this.video.channel === 'string') {
+      channelId = this.video.channel;
+    } else {
+      channelId = this.video.channel.id;
+    }
+
+    this.channelPermissionsService.getIsAdmin(channelId).subscribe(isAdmin => {
+      const ownerId = typeof this.video.owner === 'string' ?  this.video.owner : this.video.owner.id;
+      this.hasReUploadPermissions = isAdmin || ownerId === this.userService.currentUser.id;
+    });
+  }
 }
